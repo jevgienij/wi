8b01a3e1021c820423f5f0644b497742851a1daf
jevgienij <6131866+jevgienij@users.noreply.github.com>
2022-05-06 15:08:40 +0200

    Triangular numbers using math approach and generator approach.

---
+++ src/wi/triangular.py
@@ -0,0 +1,43 @@
+from math import sqrt
+import itertools
+
+
+def check_if_triangular_using_math_eq(x: int) -> bool:
+    """
+    1, 3, 6, 10, 15, 21, 1 + 2 + 3 + ... + (n-1) + n
+
+    T = (n+1)*n/2
+    2*T = n^2 +n
+    n^2 + n - 2*T = 0
+
+    delta = 1 + 8T
+
+    root = (-1 + sqrt(1 + 8T))/2
+
+    O(1) - time complexity
+    O(1) - space complexity
+
+    :param x: number to test if it's triangular
+    :return: True if given x is a triangular number
+    """
+    try:
+        root = (-1 + sqrt(1 + 8*x))/2
+    except ValueError:
+        return False
+    return root.is_integer()
+
+
+def gen_triangular_number():
+    return itertools.accumulate(itertools.count(0))
+
+
+def check_if_triangular_using_generator(x: int) -> bool:
+    triangular_gen = gen_triangular_number()
+    triangular_number = next(triangular_gen)
+    while True:
+        if triangular_number > x:
+            return False
+        elif triangular_number == x:
+            return True
+        else:
+            triangular_number = next(triangular_gen)
--- tests/test_triangular.py
+++ tests/test_triangular.py
@@ -1,6 +1,46 @@
 import pytest
+import wi.triangular as tri
+
+
+def valid_triangular_numbers():
+    ret = [
+        0,
+        1,
+        3,
+        6,
+        10,
+        15,
+        21,
+    ]
+    return ret
+
+
+def invalid_triangular_numbers():
+    ret = [
+        -1,
+        -15,
+        16,
+        17,
+        15.01,
+    ]
+    return ret
 
 
 class TestTriangular:
-    def test_given_number_return_true_if_triangular(self):
-        pass
+    @pytest.mark.parametrize("valid_triangular_number", valid_triangular_numbers())
+    def test_given_number_return_true_if_triangular(self, valid_triangular_number):
+        assert tri.check_if_triangular_using_math_eq(valid_triangular_number)
+
+    @pytest.mark.parametrize("invalid_triangular_number", invalid_triangular_numbers())
+    def test_given_number_return_false_if_not_triangular(self, invalid_triangular_number):
+        assert not tri.check_if_triangular_using_math_eq(invalid_triangular_number)
+
+    @pytest.mark.parametrize("valid_triangular_number", valid_triangular_numbers())
+    def test_given_number_return_true_if_triangular_using_generator(self, valid_triangular_number):
+        assert tri.check_if_triangular_using_generator(valid_triangular_number)
+
+    @pytest.mark.parametrize("invalid_triangular_number", invalid_triangular_numbers())
+    def test_given_number_return_false_if_not_triangular_using_generator(
+            self, invalid_triangular_number
+    ):
+        assert not tri.check_if_triangular_using_generator(invalid_triangular_number)
